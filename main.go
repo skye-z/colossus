5aca2c02cd80eaacef3e0481aa8c6ff174dcd58f
skay <skai-zhang@hotmail.com>
2023-08-14 21:07:53 +0800

    update config

--- main.go
+++ main.go
@@ -8,23 +8,38 @@ import (
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
-//go:embed all:frontend/dist
+// 引入静态资源
+// go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
+	// 创建应用程序实例
 	app := NewApp()
-
-	// Create application with options
+	// 运行应用程序
 	err := wails.Run(&options.App{
-		Title:  "colossus",
-		Width:  1024,
-		Height: 768,
+		// 窗口标题
+		Title: "Colossus",
+		// 默认宽度
+		Width: 1024,
+		// 默认高度
+		Height: 700,
+		// 最小宽度
+		MinWidth: 1024,
+		// 最小高度
+		MinHeight: 700,
+		// 静态资源服务
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+		// 背景颜色
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		// 应用启动事件
+		OnStartup:     app.startup,
+		OnBeforeClose: app.beforeClose,
+		// 调试
+		Debug: options.Debug{
+			OpenInspectorOnStartup: false,
+		},
 		Bind: []interface{}{
 			app,
 		},
