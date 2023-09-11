58c06f4da1b53fadc7ee848fa5d59c1d5f3111e0
skay <skai-zhang@hotmail.com>
2023-09-11 18:03:59 +0800

    add host cert cenerate function

--- backend/common/security.go
+++ backend/common/security.go
@@ -1,12 +1,20 @@
 package common
 
 import (
-	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
+	"log"
+
+	"crypto/aes"
+	"crypto/rsa"
+
+	"golang.org/x/crypto/ssh"
 )
 
 // AES加密
-func encrypt(plainText []byte, key []byte, iv []byte) ([]byte, error) {
+func EncryptAES(plainText []byte, key []byte, iv []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -20,7 +28,7 @@ func encrypt(plainText []byte, key []byte, iv []byte) ([]byte, error) {
 }
 
 // AES解密
-func decrypt(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
+func DecryptAES(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -31,4 +39,45 @@ func decrypt(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
 	mode.CryptBlocks(plainText, ciphertext)
 
 	return plainText, nil
+}
+
+type HostCert struct {
+	Type       string `json:"type"`
+	Level      int16  `json:"level"`
+	PublicKey  string `json:"publicKey"`
+	PrivateKey string `json:"privateKey"`
+}
+
+// 生成密钥
+func GenerateHostCert(level int) *HostCert {
+	// 生成RSA私钥
+	privateKey, err := rsa.GenerateKey(rand.Reader, level)
+	if err != nil {
+		log.Println("[Cert]", err)
+		return nil
+	}
+	// 获取RSA公钥
+	publicKey := privateKey.PublicKey
+
+	sshPub, err := ssh.NewPublicKey(&publicKey)
+	if err != nil {
+		log.Println("[Cert]", err)
+		return nil
+	}
+	sshRsaBytes := ssh.MarshalAuthorizedKey(sshPub)
+
+	// 将私钥和公钥转为字节切片
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	// 将私钥和公钥编码为PEM格式
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privateKeyBytes,
+	})
+	hostCert := &HostCert{
+		Type:       "RSA",
+		Level:      2048,
+		PrivateKey: string(privateKeyPEM),
+		PublicKey:  string(sshRsaBytes),
+	}
+	return hostCert
 }
